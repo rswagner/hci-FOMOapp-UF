cbe3992184a329c3936f20c7420f2b5d71abd789
Ricardo Wagner <ricardowagner.op@gmail.com>
2018-03-30 17:40:17 -0400

    Added some functionality to the main pages

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,3 +1,11 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
+  $('.tabs').tabs();
+  $('ul.tabs').tabs({
+  swipeable : true,
+});
+  instance.select('tab_id');
+  instance.updateTabIndicator();
+  instance.destroy();
+
 })
