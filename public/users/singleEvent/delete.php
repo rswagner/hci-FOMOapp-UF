079e5727661b9fd16ca1b100b741cd7a2ab02783
Bryan Rivera <brbriv@gmail.com>
2018-04-02 21:09:36 -0400

    Fixed delete database bug.

--- public/users/singleEvent/delete.php
+++ public/users/singleEvent/delete.php
@@ -5,15 +5,15 @@
 if(!isset($_GET['id'])) {
   redirect_to(url_for('/users/allEvents.php'));
 }
-$id = $_GET['id'] ;
-$sql = "DELETE FROM users.Events WHERE EventID='" . $id . "'";
-$result = mysqli_query($db, $sql);
 
-if ($result){
-  redirect_to(url_for('/users/allEvents.php'));
-}else {
-  // UPDATE failed
-  echo mysqli_error($db);
-}
+  $id = $_GET['id'];
+  $sql = "DELETE FROM users.Events WHERE EventID='" . $id . "'";
+  $result = mysqli_query($db, $sql);
+
+  if ($result){
+    redirect_to(url_for('/users/allEvents.php'));
+  }else {
+    echo mysqli_error($db);
+  }
 
 ?>
