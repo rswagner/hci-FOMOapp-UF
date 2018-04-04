9fb8323f555c7c15b6c607ebf5e6b628578439e1
Ricardo Wagner <ricardowagner.op@gmail.com>
2018-04-03 20:24:12 -0400

    Merge conflict cleanup

--- public/users/profile/profile.php
+++ public/users/profile/profile.php
@@ -22,19 +22,9 @@ if ($_SESSION['type'] == "student"){
 }
 // org
 else {
-<<<<<<< HEAD
-  $sql = "SELECT OrganizationDescription FROM users.Organizations WHERE ";
-  $sql .= "OrganizationID = '" . $_SESSION['id'] . "'";
-  $organizationInfo_set = mysqli_query($db, $sql);
-||||||| merged common ancestors
-  $sql = "SELECT OrganizationDescription FROM users.Organizations WHERE ";
-  $sql .= "OrganizationID = '" . $_SESSION['id'] . "'"; 
-  $organizationInfo_set = mysqli_query($db, $sql); 
-=======
   $sql = "SELECT ProfilePic, OrganizationDescription FROM users.Organizations WHERE ";
   $sql .= "OrganizationID = '" . $_SESSION['id'] . "'";
   $organizationInfo_set = mysqli_query($db, $sql);
->>>>>>> aedc52c5f3108b1b01b3d422661fc37775576012
 
   $info = mysqli_fetch_assoc($organizationInfo_set);
   mysqli_free_result($organizationInfo_set);
@@ -46,117 +36,6 @@ else {
 //$content = $row['https://s3.us-east-2.amazonaws.com/hci-fomo/logan.jpg'];
 //readfile($content);
 if (is_post_request()){
-<<<<<<< HEAD
-	$profile_pic;
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename( $_FILES["fileToUpload"]["name"] );
-	$profile_pic['ProfilePic'] = $target_dir . basename( $_FILES["fileToUpload"]["name"] );
-	$sql = "UPDATE users.Users SET ";
-	$sql .= "ProfilePic='" . $profile_pic['ProfilePic'] . "' ";
-    $sql .= "WHERE UserID='" . $_SESSION['id'] . "' ";
-    $sql .= "LIMIT 1";
-	$result = mysqli_query($db, $sql);
-
-	$uploadOk = 1;
-	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	// Check if image file is a actual image or fake image
-	if(isset($_POST["submit"])) {
-		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		if($check !== false	) {
-			echo "File is an image - " . $check["mime"] . ".";
-			$uploadOk = 1;
-		} else {
-			echo "File is not an image.";
-			$uploadOk = 0;
-		}
-	}
-	// Check if file already exists
-	if (file_exists($target_file)) {
-		echo "Sorry, file already exists.";
-		$uploadOk = 0;
-	}
-	// Check file size
-	if ($_FILES["fileToUpload"]["size"] > 500000) {
-		echo "Sorry, your file is too large.";
-		$uploadOk = 0;
-	}
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-	&& $imageFileType != "gif" ) {
-		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		$uploadOk = 0;
-	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-		echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-	}
-	else {
-		if( move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		}
-		else {
-			echo "Sorry, there was an error uploading your file.";
-		}
-	}
-}
-?>
-||||||| merged common ancestors
-	$profile_pic; 
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename( $_FILES["fileToUpload"]["name"] );
-	$profile_pic['ProfilePic'] = $target_dir . basename( $_FILES["fileToUpload"]["name"] );
-	$sql = "UPDATE users.Users SET ";
-	$sql .= "ProfilePic='" . $profile_pic['ProfilePic'] . "' ";
-    $sql .= "WHERE UserID='" . $_SESSION['id'] . "' ";
-    $sql .= "LIMIT 1";	
-	$result = mysqli_query($db, $sql);
-
-	$uploadOk = 1;
-	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	// Check if image file is a actual image or fake image
-	if(isset($_POST["submit"])) {
-		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		if($check !== false	) {
-			echo "File is an image - " . $check["mime"] . ".";
-			$uploadOk = 1;
-		} else {
-			echo "File is not an image.";
-			$uploadOk = 0;
-		}
-	}
-	// Check if file already exists
-	if (file_exists($target_file)) {
-		echo "Sorry, file already exists.";
-		$uploadOk = 0;
-	}
-	// Check file size
-	if ($_FILES["fileToUpload"]["size"] > 500000) {
-		echo "Sorry, your file is too large.";
-		$uploadOk = 0;
-	}
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-	&& $imageFileType != "gif" ) {
-		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-		$uploadOk = 0;
-	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-		echo "Sorry, your file was not uploaded.";
-	// if everything is ok, try to upload file
-	} 
-	else {
-		if( move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		} 
-		else {
-			echo "Sorry, there was an error uploading your file.";
-		}	
-	}
-}	
-?>		
-=======
   if ($_FILES["fileToUpload"]["name"]){
   $target_dir = "uploads/";
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -229,7 +108,6 @@ if (is_post_request()){
     }
   }
 ?>
->>>>>>> aedc52c5f3108b1b01b3d422661fc37775576012
 
 <?php include(SHARED_PATH . '/user_header.php'); ?>
 
@@ -274,7 +152,7 @@ if (is_post_request()){
                 <td><?php echo $savedEvent['EventName'] ?></td>
                 <!-- Dynamically go through a for loop and echo the id in the url! -->
                 <td>
-                    <?php 
+                    <?php
                         $date = str_replace("-","",$savedEvent['Date']);
                         $location = $savedEvent['Location'];
                         $startTime = str_replace(":","",$savedEvent['StartTime']);
