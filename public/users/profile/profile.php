056501b7c26fe67e3f006f9f4b0a95d104f9c93d
Ricardo Wagner <ricardowagner.op@gmail.com>
2018-04-03 21:19:21 -0400

    Cleaned up file directory

--- public/users/profile/profile.php
+++ public/users/profile/profile.php
@@ -112,18 +112,9 @@ if (is_post_request()){
 <?php include(SHARED_PATH . '/user_header.php'); ?>
 
 <head>
-    <meta charset="utf-8">
-    <title>FOMO UF APP</title>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <!-- Compiled and minified JavaScript -->
+    <title>Profile View</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!--Custom imports !-->
     <link rel="stylesheet" href="<?=WWW_ROOT?>/css/profile.css">
 </head>
 
@@ -199,7 +190,6 @@ if (is_post_request()){
     </footer>
   </body>
 </html>
-
 <script>
     var inputs = document.querySelectorAll( '.inputfile' );
     Array.prototype.forEach.call( inputs, function( input ) {
